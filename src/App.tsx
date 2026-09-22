ce64155186b471339063c174a088eb27177d1c99
aryone120-create <aryone120@gmail.com>
2026-09-22 11:39:01 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom'
 import Website from './pages/Website'
 import Poster from './pages/Poster'
 
